0258286d6f981d04e4f6352809e07d8c8aa8b9b2
HueyBayer <140875818+HueyBayer@users.noreply.github.com>
2023-07-29 16:06:58 +0000

    added and connected to a database on MySQL

---
+++ database.py
@@ -0,0 +1,3 @@
+from sqlalchemy import create_engine
+
+engine = create_engine("mysql+pymysql://71ru4roe600vesg5b39n:pscale_pw_yQQ9K8je6dGBx6tQiBBNfEeUWyH8RcMaKvRKCTyiB7j@aws.connect.psdb.cloud/databasedrivenwebsite?charset=utf8mb4")
