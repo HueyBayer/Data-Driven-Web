65c237a70d3af297e850f8aeb07c347b4a564bda
HueyBayer <140875818+HueyBayer@users.noreply.github.com>
2023-08-03 06:57:09 +0000

    Added dynamic pages for jobs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 from sqlalchemy import text
 
 app = Flask(__name__)
@@ -14,9 +14,13 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
-@app.route("/newhome")
-def new_home():
-  return render_template("home-original.html")
+@app.route("/apply/<id>")
+def show_job(id):
+  job_lists = load_job_from_db(id)
+  if not job_lists:
+    return "Not Found", 404
+  return render_template("apply.html", job=job_lists)
   
 if __name__ == "__main__":
   app.run(host = '0.0.0.0', debug = True)
+
--- database.py
+++ database.py
@@ -7,8 +7,17 @@ engine = create_engine(database_connection, connect_args={"ssl": {"ssl_ca":"/etc
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
+    result = conn.execute(text("SELECT * FROM jobs"))
     jobs = []
     for row in result.all():
       jobs.append(row._mapping)
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), {"val": id})
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(zip("titlerq", rows[0]))
